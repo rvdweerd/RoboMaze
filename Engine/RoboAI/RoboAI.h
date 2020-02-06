a5a5a3ae87dd87083a21f2b9d988582e21d5ced9
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-02-06 21:45:14 +0100

    smarter routing (priority to stack element that was inserted earlier)

--- Engine/RoboAI/RoboAI.h
+++ Engine/RoboAI/RoboAI.h
@@ -10,6 +10,48 @@
 #include <queue>
 #include <fstream>
 
+// returns angle in units of pi/2 (90deg you pleb)
+inline int GetAngleBetween(const Direction& d1, const Direction& d2)
+{
+	constexpr std::array<int, 4> map = { 0,2,3,1 };
+	return map[d2.GetIndex()] - map[d1.GetIndex()];
+}
+// angle is in units of pi/2 (90deg you pleb)
+inline Vei2 GetRotated90(const Vei2& v, int angle)
+{
+	Vei2 vo;
+	switch (angle)
+	{
+	case -3:
+		vo.x = -v.y;
+		vo.y = v.x;
+		break;
+	case -2:
+		vo = -v;
+		break;
+	case -1:
+		vo.x = v.y;
+		vo.y = -v.x;
+		break;
+	case 0:
+		vo = v;
+		break;
+	case 1:
+		vo.x = -v.y;
+		vo.y = v.x;
+		break;
+	case 2:
+		vo = -v;
+		break;
+	case 3:
+		vo.x = v.y;
+		vo.y = -v.x;
+		break;
+	default:
+		assert("Bad angle in Dir rotation!" && false);
+	}
+	return vo;
+}
 enum class RoboDir
 {
 	NORTH=0,
@@ -28,7 +70,7 @@ struct RoboPosDir // Data structure for tracking the state (position & orientati
 	RoboDir dir = RoboDir::EAST; // orientation
 };
 
-// test class
+// test classes
 class RoboAI_rvdw
 {
 	using TT = TileMap::TileType;
@@ -43,6 +85,7 @@ public:
 	int SquareDanceToggle = 1;
 	std::queue<Robo::Action> ReturnFromSquareDanceQueue;
 	//static constexpr bool implemented = false;
+	int lastPos = 0;
 	Action Plan(std::array<TT, 3> view)
 	{
 		RecordFieldView(view);
@@ -103,7 +146,8 @@ public:
 			}
 			target = stack.top().first;
 			returnPos = stack.top().second; stack.pop();
-		} while (!IsUFG(target));
+		} while (!IsUFG(target) || visited.find(target) != visited.end());
+		visited.insert(target);
 
 		if (IsNeighbor(target)) // Move Ahead
 		{
@@ -283,6 +327,8 @@ private: //Exploration control
 											roboPosDir.posIndex + 1,
 											roboPosDir.posIndex + fieldWidth,
 											roboPosDir.posIndex - 1 };
+		std::pair<int, int> elementToTop;
+		bool swap = false;
 		for (size_t i = 0; i < 4; i++)
 		{
 			int index = indicesNESW[i];
@@ -290,16 +336,42 @@ private: //Exploration control
 			if (it == fieldMap.end()) // unexplored
 			{
 				stack.push({ index , roboPosDir.posIndex });
-				visited.insert(index);
+				//visited.insert(index);
 				count++;
 			}
 			else if ((it->second == TT::Floor || it->second == TT::Goal) && // neighbor is accessible
 				(visited.find(index) == visited.end()))					// neighbor hasn't been visited before
 			{
-				stack.push({ index, roboPosDir.posIndex });
-				visited.insert(index);
-				count++;
+				if (visited.find(index) == visited.end())					// neighbor hasn't been visited before
+				{
+					stack.push({ index, roboPosDir.posIndex });
+					//visited.insert(index);
+					count++;
+				}
+				else if (index != lastPos)
+				{
+					elementToTop = { index, roboPosDir.posIndex };
+					swap = true;
+				}
 			}
+		}
+		if (swap)
+		{
+			std::stack<std::pair<int, int>> cStack;
+			while (!stack.empty())
+			{
+				if (stack.top() != elementToTop)
+				{
+					cStack.push(stack.top());
+				}
+				stack.pop();
+			}
+			while (!cStack.empty())
+			{
+				stack.push(cStack.top());
+				cStack.pop();
+			}
+			stack.push(elementToTop);
 		}
 		return count;
 	}
@@ -358,6 +430,7 @@ private: //Exploration control
 	}
 	Robo::Action ProcessInstruction()
 	{
+		lastPos = roboPosDir.posIndex;
 		Robo::Action nextaction = instructionQueue.front(); instructionQueue.pop();
 		if (nextaction == Robo::Action::Done) return nextaction;
 		if (nextaction == Robo::Action::MoveForward)
@@ -398,8 +471,216 @@ private: //Exploration control
 	std::vector<RoboPosDir> path;
 	std::queue<Action> instructionQueue;
 };
+class RoboAI_chili
+{
+	using TT = TileMap::TileType;
+	using Action = Robo::Action;
+public:
+	class Node
+	{
+	public:
+		TT GetType() const
+		{
+			return type;
+		}
+		void SetType(TT type_in)
+		{
+			assert(type == TT::Invalid);
+			type = type_in;
+		}
+		void ClearCost()
+		{
+			cost = std::numeric_limits<int>::max();
+		}
+		bool UpdateCost(int new_cost, const Vei2& new_prev = { -1,-1 })
+		{
+			if (new_cost < cost)
+			{
+				cost = new_cost;
+				prev = new_prev;
+				return true;
+			}
+			return false;
+		}
+		int GetCost() const
+		{
+			return cost;
+		}
+		const Vei2& GetPrev() const
+		{
+			return prev;
+		}
+	private:
+		TT type = TT::Invalid;
+		Vei2 prev;
+		int cost = std::numeric_limits<int>::max();
+	};
+public:
+	RoboAI_chili()
+		:
+		nodes(gridWidth* gridHeight),
+		pos(gridWidth / 2, gridHeight / 2), // start in middle
+		visit_extent(-1, -1, -1, -1)
+	{
+		// prime the pathing pump
+		path.push_back(pos + dir);
+		ResetExtents(pos);
+	}
+	// this signals to the system whether the debug AI can be used
+	static constexpr bool implemented = true;
+	Action Plan(std::array<TT, 3> view)
+	{
+		// return DONE if standing on the goal
+		if (At(pos).GetType() == TT::Goal)
+		{
+			return Action::Done;
+		}
+		// first update map
+		UpdateMap(view);
+		// check if we have revealed the target unknown square
+		if (At(path.back()).GetType() != TT::Invalid)
+		{
+			// compute new path
+			if (!ComputePath())
+			{
+				// if ComputePath() returns false, impossible to reach goal
+				return Action::Done;
+			}
+		}
+		// find our position in path sequence
+		const auto i = std::find(path.begin(), path.end(), pos);
+		// move to next position in sequence
+		return MoveTo(*std::next(i));
+	}
+private:
+	bool ComputePath()
+	{
+		// create priority queue for 'frontier'
+		std::deque<Vei2> frontier;
 
-// Debug  / visualization class
+		// clear costs
+		for (Vei2 cp = visit_extent.TopLeft(); cp.y <= visit_extent.bottom; cp.y++)
+		{
+			for (cp.x = visit_extent.left; cp.x <= visit_extent.right; cp.x++)
+			{
+				At(cp).ClearCost();
+			}
+		}
+
+		ResetExtents(pos);
+
+		// clear path sequence
+		path.clear();
+
+		// add current tile to frontier to0 start the ball rollin
+		At(pos).UpdateCost(0);
+		frontier.emplace_back(pos);
+		AdjustExtents(pos);
+
+		while (!frontier.empty())
+		{
+			const auto base = frontier.front();
+			frontier.pop_front();
+
+			auto dir = Direction::Up();
+			for (int i = 0; i < 4; i++, dir.RotateClockwise())
+			{
+				const auto nodePos = base + dir;
+				auto& node = At(nodePos);
+				if (node.GetType() == TT::Invalid || node.GetType() == TT::Goal)
+				{
+					// then construct path and return
+					path.push_back(nodePos);
+					auto trace = base;
+					while (At(trace).GetPrev() != Vei2{ -1,-1 })
+					{
+						// add it to the path
+						path.push_back(trace);
+						// walk to next pos in path
+						trace = At(trace).GetPrev();
+					}
+					// add it to the path
+					path.push_back(trace);
+					// gotta reverse that shit
+					std::reverse(path.begin(), path.end());
+					return true;
+				}
+				else if (node.GetType() == TT::Floor)
+				{
+					if (node.UpdateCost(At(base).GetCost() + 1, base))
+					{
+						frontier.push_back(nodePos);
+						AdjustExtents(nodePos);
+					}
+				}
+			}
+		}
+		// cannot reach goal
+		return false;
+	}
+	void UpdateMap(const std::array<TT, 3>& view)
+	{
+		auto scan_pos = pos + dir + dir.GetRotatedCounterClockwise();
+		const auto scan_delta = dir.GetRotatedClockwise();
+		for (int i = 0; i < 3; i++, scan_pos += scan_delta)
+		{
+			auto& node = At(scan_pos);
+			const auto type = node.GetType();
+			if (type != TT::Invalid)
+			{
+				assert(type == view[i]);
+				continue;
+			}
+			node.SetType(view[i]);
+		}
+	}
+	Node& At(const Vei2& pos)
+	{
+		return nodes[pos.x + pos.y * gridWidth];
+	}
+	Action MoveTo(const Vei2& target)
+	{
+		const Direction delta = Direction(target - pos);
+		if (delta == dir)
+		{
+			pos += dir;
+			return Action::MoveForward;
+		}
+		else if (dir.GetRotatedClockwise() == delta)
+		{
+			dir.RotateClockwise();
+			return Action::TurnRight;
+		}
+		else
+		{
+			dir.RotateCounterClockwise();
+			return Action::TurnLeft;
+		}
+	}
+	void AdjustExtents(const Vei2& pos)
+	{
+		visit_extent.left = std::min(pos.x, visit_extent.left);
+		visit_extent.right = std::max(pos.x, visit_extent.right);
+		visit_extent.top = std::min(pos.y, visit_extent.top);
+		visit_extent.bottom = std::max(pos.y, visit_extent.bottom);
+	}
+	void ResetExtents(const Vei2& pos)
+	{
+		visit_extent = { pos.x + 1,pos.x - 1,pos.y + 1, pos.y - 1 };
+	}
+private:
+	// grid dimensions 2x the max dimensions
+	// (because we don't know where we start!)
+	static constexpr int gridWidth = 2000;
+	static constexpr int gridHeight = 2000;
+	Vei2 pos;
+	std::vector<Vei2> path;
+	Direction dir = Direction::Up();
+	std::vector<Node> nodes;
+	// inclusive!
+	RectI visit_extent;
+};
+// Debug  / visualization classes
 class RoboAIDebug_rvdw
 {
 	using TT = TileMap::TileType;
@@ -417,6 +698,7 @@ public:
 	int SquareDanceToggle = 1;
 	std::queue<Robo::Action> ReturnFromSquareDanceQueue;
 	static constexpr bool implemented = true;
+	int lastPos = 0;
 	Action Plan(std::array<TT, 3> view)
 	{
 		RecordFieldView(view);
@@ -483,8 +765,8 @@ public:
 				}
 				target = stack.top().first;
 				returnPos = stack.top().second; stack.pop();
-			} while (!IsUFG(target));
-
+			} while (!IsUFG(target) || visited.find(target)!=visited.end());
+			visited.insert(target);
 			if (IsNeighbor(target)) // Move Ahead
 			{
 				RoboDir targetDir = BuildInstructionQueue_MoveToAdjacentCell(target);
@@ -659,7 +941,7 @@ private: //Field info
 		}
 		return RoboDir::count;
 	}
-	static constexpr int maxFieldSideLength = 2000;
+	static constexpr int maxFieldSideLength = 1004;
 	static constexpr int fieldWidth = 2 * maxFieldSideLength + 1;
 	static constexpr int fieldHeight = fieldWidth;
 	static constexpr int nField = fieldWidth * fieldHeight;
@@ -674,6 +956,8 @@ private: //Exploration control
 											roboPosDir.posIndex + 1, 
 											roboPosDir.posIndex + fieldWidth,
 											roboPosDir.posIndex - 1 };
+		std::pair<int, int> elementToTop;
+		bool swap = false;
 		for (size_t i = 0; i < 4; i++)
 		{
 			int index = indicesNESW[i];
@@ -681,16 +965,41 @@ private: //Exploration control
 			if (it == fieldMap.end()) // unexplored
 			{
 				stack.push({ index , roboPosDir.posIndex});
-				visited.insert(index);
+				//visited.insert(index);
 				count++;
 			}
-			else if ((it->second == TT::Floor || it->second == TT::Goal) && // neighbor is accessible
-				(visited.find(index) == visited.end()) )					// neighbor hasn't been visited before
+			else if (it->second == TT::Floor || it->second == TT::Goal) // neighbor is accessible
 			{
-				stack.push({ index, roboPosDir.posIndex});
-				visited.insert(index);
-				count++;
+				if (visited.find(index) == visited.end())					// neighbor hasn't been visited before
+				{	
+					stack.push({ index, roboPosDir.posIndex });
+					//visited.insert(index);
+					count++;
+				}
+				else if (index != lastPos)
+				{
+					elementToTop = { index, roboPosDir.posIndex };
+					swap = true;
+				}
 			}
+		}
+		if (swap)
+		{
+			std::stack<std::pair<int, int>> cStack;
+			while (!stack.empty())
+			{
+				if (stack.top() != elementToTop)
+				{
+					cStack.push(stack.top());
+				}
+				stack.pop();
+			}
+			while (!cStack.empty())
+			{
+				stack.push(cStack.top());
+				cStack.pop();
+			}
+			stack.push(elementToTop);
 		}
 		return count;
 	}
@@ -749,6 +1058,7 @@ private: //Exploration control
 	}
 	Robo::Action ProcessInstruction()
 	{
+		lastPos = roboPosDir.posIndex;
 		Robo::Action nextaction = instructionQueue.front(); instructionQueue.pop();
 		if (nextaction == Robo::Action::Done) return nextaction;
 		if (nextaction == Robo::Action::MoveForward)
@@ -791,6 +1101,268 @@ private: //Support data
 	DebugControls& dc;
 	std::ofstream out_file;
 };
+class RoboAIDebug_chili
+{
+	using TT = TileMap::TileType;
+	using Action = Robo::Action;
+public:
+	class Node
+	{
+	public:
+		TT GetType() const
+		{
+			return type;
+		}
+		void SetType(TT type_in)
+		{
+			assert(type == TT::Invalid);
+			type = type_in;
+		}
+		void ClearCost()
+		{
+			cost = std::numeric_limits<int>::max();
+		}
+		bool UpdateCost(int new_cost, const Vei2& new_prev = { -1,-1 })
+		{
+			if (new_cost < cost)
+			{
+				cost = new_cost;
+				prev = new_prev;
+				return true;
+			}
+			return false;
+		}
+		int GetCost() const
+		{
+			return cost;
+		}
+		const Vei2& GetPrev() const
+		{
+			return prev;
+		}
+	private:
+		TT type = TT::Invalid;
+		Vei2 prev;
+		int cost = std::numeric_limits<int>::max();
+	};
+public:
+	RoboAIDebug_chili(DebugControls& dc)
+		:
+		nodes(gridWidth* gridHeight),
+		pos(gridWidth / 2, gridHeight / 2), // start in middle
+		dc(dc),
+		angle(GetAngleBetween(dir, dc.GetRobotDirection())),
+		visit_extent(-1, -1, -1, -1)
+	{
+		// prime the pathing pump
+		path.push_back(pos + dir);
+		VisualInit();
+		ResetExtents(pos);
+	}
+	// this signals to the system whether the debug AI can be used
+	static constexpr bool implemented = true;
+	Action Plan(std::array<TT, 3> view)
+	{
+		// return DONE if standing on the goal
+		if (At(pos).GetType() == TT::Goal)
+		{
+			const auto lock = dc.AcquireGfxLock();
+			ClearPathMarkings();
+			return Action::Done;
+		}
+		// first update map
+		UpdateMap(view);
+		// check if we have revealed the target unknown square
+		if (At(path.back()).GetType() != TT::Invalid)
+		{
+			// compute new path
+			if (!ComputePath())
+			{
+				// if ComputePath() returns false, impossible to reach goal
+				return Action::Done;
+			}
+		}
+		// find our position in path sequence
+		const auto i = std::find(path.begin(), path.end(), pos);
+		// move to next position in sequence
+		return MoveTo(*std::next(i));
+	}
+private:
+	bool ComputePath()
+	{
+		{
+			const auto lock = dc.AcquireGfxLock();
+			ClearPathMarkings();
+		}
+		// create priority queue for 'frontier'
+		std::deque<Vei2> frontier;
+
+		// clear costs
+		for (Vei2 cp = visit_extent.TopLeft(); cp.y <= visit_extent.bottom; cp.y++)
+		{
+			for (cp.x = visit_extent.left; cp.x <= visit_extent.right; cp.x++)
+			{
+				At(cp).ClearCost();
+			}
+		}
+
+		ResetExtents(pos);
+
+		// clear path sequence
+		path.clear();
+
+		// add current tile to frontier to0 start the ball rollin
+		At(pos).UpdateCost(0);
+		frontier.emplace_back(pos);
+		AdjustExtents(pos);
+
+		while (!frontier.empty())
+		{
+			const auto base = frontier.front();
+			frontier.pop_front();
+
+			auto dir = Direction::Up();
+			for (int i = 0; i < 4; i++, dir.RotateClockwise())
+			{
+				const auto nodePos = base + dir;
+				auto& node = At(nodePos);
+				if (node.GetType() == TT::Invalid || node.GetType() == TT::Goal)
+				{
+					// found our target, first mark it
+					dc.MarkAt(ToGameSpace(nodePos), { 96,96,0,0 });
+					// then construct path and return
+					path.push_back(nodePos);
+					auto trace = base;
+					while (At(trace).GetPrev() != Vei2{ -1,-1 })
+					{
+						// mark sequence for visualiztion yellow
+						dc.MarkAt(ToGameSpace(trace), { 96,96,96,0 });
+						// add it to the path
+						path.push_back(trace);
+						// walk to next pos in path
+						trace = At(trace).GetPrev();
+					}
+					// mark sequence for visualiztion yellow
+					dc.MarkAt(ToGameSpace(trace), { 96,96,96,0 });
+					// add it to the path
+					path.push_back(trace);
+					// gotta reverse that shit
+					std::reverse(path.begin(), path.end());
+					return true;
+				}
+				else if (node.GetType() == TT::Floor)
+				{
+					if (node.UpdateCost(At(base).GetCost() + 1, base))
+					{
+						frontier.push_back(nodePos);
+						AdjustExtents(nodePos);
+						// mark each tile visited by my dik
+						dc.MarkAt(ToGameSpace(nodePos), { Colors::Green,32 });
+						dc.WaitOnTick();
+					}
+				}
+			}
+		}
+		// cannot reach goal
+		return false;
+	}
+	void UpdateMap(const std::array<TT, 3>& view)
+	{
+		auto scan_pos = pos + dir + dir.GetRotatedCounterClockwise();
+		const auto scan_delta = dir.GetRotatedClockwise();
+		for (int i = 0; i < 3; i++, scan_pos += scan_delta)
+		{
+			auto& node = At(scan_pos);
+			const auto type = node.GetType();
+			if (type != TT::Invalid)
+			{
+				assert(type == view[i]);
+				continue;
+			}
+			node.SetType(view[i]);
+			dc.ClearAt(ToGameSpace(scan_pos));
+		}
+	}
+	Node& At(const Vei2& pos)
+	{
+		return nodes[pos.x + pos.y * gridWidth];
+	}
+	Action MoveTo(const Vei2& target)
+	{
+		const Direction delta = Direction(target - pos);
+		if (delta == dir)
+		{
+			pos += dir;
+			return Action::MoveForward;
+		}
+		else if (dir.GetRotatedClockwise() == delta)
+		{
+			dir.RotateClockwise();
+			return Action::TurnRight;
+		}
+		else
+		{
+			dir.RotateCounterClockwise();
+			return Action::TurnLeft;
+		}
+	}
+	Vei2 ToGameSpace(const Vei2& pos_in) const
+	{
+		return GetRotated90(pos_in - pos, angle) + dc.GetRobotPosition();
+	}
+	Vei2 ToMapSpace(const Vei2& pos_in) const
+	{
+		return GetRotated90(pos_in - dc.GetRobotPosition(), -angle) + pos;
+	}
+	void VisualInit()
+	{
+		if (!visual_init_done)
+		{
+			auto l = dc.AcquireGfxLock();
+			dc.MarkAll({ Colors::Black,96 });
+			visual_init_done = true;
+		}
+	}
+	void ClearPathMarkings()
+	{
+		dc.ForEach([this](const Vei2& p, DebugControls& dc)
+			{
+				if (At(ToMapSpace(p)).GetType() != TileMap::TileType::Invalid)
+				{
+					dc.ClearAt(p);
+				}
+			});
+	}
+	void AdjustExtents(const Vei2& pos)
+	{
+		visit_extent.left = std::min(pos.x, visit_extent.left);
+		visit_extent.right = std::max(pos.x, visit_extent.right);
+		visit_extent.top = std::min(pos.y, visit_extent.top);
+		visit_extent.bottom = std::max(pos.y, visit_extent.bottom);
+	}
+	void ResetExtents(const Vei2& pos)
+	{
+		visit_extent = { pos.x + 1,pos.x - 1,pos.y + 1, pos.y - 1 };
+	}
+private:
+	DebugControls& dc;
+	bool visual_init_done = false;
+	// grid dimensions 2x the max dimensions
+	// (because we don't know where we start!)
+	static constexpr int gridWidth = 2000;
+	static constexpr int gridHeight = 2000;
+	Vei2 pos;
+	std::vector<Vei2> path;
+	Direction dir = Direction::Up();
+	std::vector<Node> nodes;
+	int angle;
+	// inclusive!
+	RectI visit_extent;
+};
+
+
 // if you name your classes different than RoboAI/RoboAIDebug, use typedefs like these
 typedef RoboAI_rvdw RoboAI;
 typedef RoboAIDebug_rvdw RoboAIDebug;
+//typedef RoboAI_chili RoboAI;
+//typedef RoboAIDebug_chili RoboAIDebug;
